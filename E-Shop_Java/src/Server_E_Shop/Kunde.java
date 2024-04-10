150e2b90fa6a8ff130498b13f452bd25eadbf8dd
simon <sbrockhoff@stud.hs-bremen.de>
2024-04-10 15:49:33 +0200

    Changes by maxim

---
+++ E-Shop_Java/src/Server_E_Shop/Kunde.java
@@ -0,0 +1,15 @@
+package Server_E_Shop;
+//Hello
+public class Kunde extends Person {
+    int plz;
+    String ort;
+    String strasse;
+    int strassenNummer;
+    public Kunde(int id, String name, String username, String password, String ort, int plz, String strasse, int strassenNummer) {
+        super(id, name, username, password);
+        this.ort = ort;
+        this.plz = plz;
+        this.strasse = strasse;
+        this.strassenNummer = strassenNummer;
+    }
+}
---
+++ E-Shop_Java/src/Server_E_Shop/Person.java
@@ -0,0 +1,15 @@
+package Server_E_Shop;
+
+public class Person {
+  String name;
+  int id;
+  String username;
+  String password;
+
+  public Person(int id, String name, String username, String password ){
+      this.name = name;
+      this.id = id;
+      this.username = username;
+      this.password = password;
+  }
+}
