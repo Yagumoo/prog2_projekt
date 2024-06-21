184e4d04661ac1f8a9d304fd7b6c909edd8f279d
Marvin <166237540+MarvinMkN@users.noreply.github.com>
2024-06-21 13:57:12 +0200

    lol

---
+++ E-Shop_Java/src/eshop/ui/gui/EShopGUI.java
@@ -0,0 +1,24 @@
+package eshop.ui.gui;
+
+import eshop.domain.E_Shop;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+
+public class EShopGUI extends JFrame {
+
+    private E_Shop eshop;
+    private JButton addButton;
+
+
+    private  void initalisieren(){
+        this.setLayout(new BorderLayout());
+
+
+
+    }
+}
